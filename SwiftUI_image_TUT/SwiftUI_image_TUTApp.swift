eadbc9fad9e199613619e0d24c841f76b38afcda
SeonJungKim <ksj083895@gmail.com>
2021-03-20 19:04:35 +0500

    Initial Commit

---
+++ SwiftUI_image_TUT/SwiftUI_image_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_image_TUTApp.swift
+//  SwiftUI_image_TUT
+//
+//  Created by 김선중 on 2021/03/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_image_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
